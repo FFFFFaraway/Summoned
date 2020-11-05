dbfb29861cbddb04f733f51e83fed0bcc90af276
FFFFFaraway <425499721@qq.com>
2020-11-05 19:41:38 +0800

    添加summoned类型与名称查询

--- front/src/components/SummonedList.vue
+++ front/src/components/SummonedList.vue
@@ -2,7 +2,7 @@
   <div>
     <ul>
       <li v-for="s in summoneds" :key="s.ID">
-        <p>ID: {{s.ID}}, Creator ID: {{s.UserID}}, Name: {{s.name}}</p>
+        <p>ID: {{s.ID}}, Creator ID: {{s.UserID}}, Type: {{s.type}}, Name: {{s.name}}, People: {{s.people}}, DDL: {{s.ddl}}</p>
         <router-link :to="'summoned/'+s.ID">details</router-link>
       </li>
     </ul>
--- front/src/pages/Summoned.vue
+++ front/src/pages/Summoned.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <h1>Summoned</h1>
+    <label for="name">Search by Name</label>
+    <input type="text" v-model="filterName" id="name">
+    <br>
+    <label>Search by Type</label>
+    <multiselect v-model="filterType" :options="options"></multiselect>
+    <hr>
     <list :summoneds="summoneds"/>
   </div>
 </template>
@@ -13,14 +19,28 @@ export default {
   },
   data() {
     return {
-      summoneds: [],
+      options: ['', '技术交流','学业探讨','社会实践','公益志愿者','游玩','其它'],
+      filterType: "",
+      filterName: "",
+      allSummoneds: [],
+    }
+  },
+  computed: {
+    summoneds() {
+      if(this.filterName == '' && this.filterType == '')
+        return this.allSummoneds
+      if(this.filterName == '')
+        return this.allSummoneds.filter(s => s.type == this.filterType)
+      if(this.filterType == '')
+        return this.allSummoneds.filter(s => s.name.includes(this.filterName))
+      return this.allSummoneds.filter(s => s.name.includes(this.filterName)).filter(s => s.type == this.filterType)
     }
   },
   mounted() {
     let that = this
     this.$axios.get('summoned')
     .then(function(response) {
-      that.summoneds = response.data
+      that.allSummoneds = response.data
     })
     .catch(function(response) {
       console.log(response)
